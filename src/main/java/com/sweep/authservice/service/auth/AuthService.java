80edef40d5dbf08934d4fdec2cda90704b125c7f
SONG YUJIN <j_jongleur@naver.com>
2022-12-01 11:52:36 +0900

    [SWEEP-103] refresh token log 확인

--- src/main/java/com/sweep/authservice/service/auth/AuthService.java
+++ src/main/java/com/sweep/authservice/service/auth/AuthService.java
@@ -147,6 +147,7 @@ public class AuthService {
 //        String originRefreshToken = tokenRequestDto.getRefreshToken();
 
         String originRefreshToken = tokenReqDTO.getRefreshToken();
+        System.out.println("@@@ originRefreshToken : " + originRefreshToken);
         // refreshToken 검증
         int refreshTokenFlag = tokenProvider.validateToken(originRefreshToken);
 
@@ -168,11 +169,15 @@ public class AuthService {
         // Redis에서 mail기반으로 refresh token을 가져옴.
         String rtkInRedis = redisService.getValues(authentication.getName());
 
+        System.out.println("@@@ rtkInRedis : " + rtkInRedis);
+
         // Cache miss가 일어났을 경우
         if (!rtkInRedis.equals(originRefreshToken)) {
             // DB에서 Member Email 를 기반으로 Refresh Token 값 가져옴
             RefreshToken refreshToken = refreshTokenRepository.findByEmail(authentication.getName())
                     .orElseThrow(() -> new BizException(MemberExceptionType.LOGOUT_MEMBER)); // 로그 아웃된 사용자
+
+            System.out.println("@@@ refreshToken : " + refreshToken);
 
             // Refresh Token 일치하는지 검사
             if (!refreshToken.getValue().equals(originRefreshToken)) {
